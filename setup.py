3b6ab8550cbc3331372fbc9d84e5a932c8b93dfe
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2022-06-07 11:05:18 +0300

    release 1.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class TestHook(test):
 
 setup(
     name='balancing-http-client',
-    version='1.1.14',
+    version='1.2.0',
     description='Balancing http client for tornado',
     url='https://github.com/hhru/balancing-http-client',
     cmdclass={
