1f729669b05db027d645aac7c06fce861dabfc25
Leonid Vinogradov <l.vinogradov@hh.ru>
2025-04-18 15:43:18 +0300

    HH-257379 use upstreams getter instead upstream

--- tests/test_mock_http_client.py
+++ tests/test_mock_http_client.py
@@ -5,8 +5,8 @@ from http_client.testing import MockHttpClient
 class TestMockHttpClient(TestBase, BalancingClientMixin):
     async def test_simple_mock(self):
         with MockHttpClient() as mock_http_client:
-            mock_http_client.add('http://test/content', body='simple')
+            mock_http_client.add('http://mock_host/content', body='simple')
 
-            response = await self.balancing_client.get_url('test', '/content')
+            response = await self.balancing_client.get_url('mock_host', '/content')
             assert response.status_code == 200
             assert response.raw_body == b'simple'
